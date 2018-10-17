fb8b7025f57109057f9d412fb82c888576b93595
朱满营 <zmy@ZMY.local>
2018-10-17 10:41:58 +0800

    Site updated: 2018-10-17 10:41:58

---
+++ static/api/js/share/combine_api.js
@@ -0,0 +1,1 @@
+window._bd_share_main.F.module("share/combine_api",function(n,a,i){var e=n("base/tangram").T,s=n("base/class").Class,o=n("share/api_base");a.CombineApi=s.create(function(n){var a=this,i=null,e=null;a.setApi=function(n,a){i=n,e=a},a._init=function(){i&&e&&i.on("sharecompleted",function(n){})}},o.ApiBase)});
